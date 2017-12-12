4557a0725e710372cfacb013f96eb03c1d3858fe
K0rTy <charllys.daiany@gmail.com>
2017-12-12 15:40:07 -0300

    Upgrade layout and toolbar in intent about

--- app/src/main/java/com/application/territoryassistant/about/AboutActivity.java
+++ app/src/main/java/com/application/territoryassistant/about/AboutActivity.java
@@ -2,6 +2,7 @@ package com.application.territoryassistant.about;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.application.territoryassistant.R;
 
@@ -11,5 +12,10 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar1);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
