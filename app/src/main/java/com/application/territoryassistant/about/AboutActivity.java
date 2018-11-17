4c2182d52110db11b42a91336ad935a67c46ac50
Charllys Fernandes <charllys.daiany@gmail.com>
2018-11-17 13:49:42 -0300

    updates statusbar with color dark

--- app/src/main/java/com/application/territoryassistant/about/AboutActivity.java
+++ app/src/main/java/com/application/territoryassistant/about/AboutActivity.java
@@ -1,10 +1,12 @@
 package com.application.territoryassistant.about;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.application.territoryassistant.R;
+
+import java.util.Objects;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -13,9 +15,9 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar1);
+        Toolbar toolbar = findViewById(R.id.toolbar1);
         setSupportActionBar(toolbar);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
     }
 }
